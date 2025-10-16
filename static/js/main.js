70726588ca0ed78e582434d7e8e2cdfe3163f987
Faywbow <onix.noxious@gmail.com>
2025-10-16 18:17:11 +0200

    Refactor image modal functionality in main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,28 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Zoom image modal functionality
     const modal = document.getElementById("imageModal");
-    const modalImg = document.getElementById("modalImage");
-    const closeBtn = document.querySelector(".close");
+    if (modal) {
+        const modalImg = document.getElementById("modalImage");
+        const closeBtn = modal.querySelector(".close");
 
-    document.querySelectorAll(".zoomable").forEach(img => {
-        img.addEventListener("click", () => {
-            modal.style.display = "flex"; // Utiliser flex pour centrer l'image
-            modalImg.src = img.src;
-            document.body.style.overflow = 'hidden'; // Empêche le défilement de la page
+        document.querySelectorAll(".zoomable").forEach(img => {
+            img.addEventListener("click", () => {
+                modal.style.display = "flex";
+                modalImg.src = img.src;
+            });
         });
-    });
 
-    // Fermer le modal
-    const closeModal = () => {
-        modal.style.display = "none";
-        document.body.style.overflow = 'auto'; // Réactive le défilement
-    };
+        const closeModal = () => {
+            modal.style.display = "none";
+        };
 
-    closeBtn.onclick = closeModal;
+        closeBtn.onclick = closeModal;
 
-    modal.onclick = function(e) {
-        if (e.target === modal) {
-            closeModal();
-        }
-    };
+        modal.addEventListener("click", (e) => {
+            if (e.target === modal) {
+                closeModal();
+            }
+        });
+    }
 });
